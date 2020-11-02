18cc421cadea9a8e7af20c74702e303133a32084
Thingyman-DEV <67653706+Thingyman-DEV@users.noreply.github.com>
2020-11-02 10:59:44 -0500

    Update countdown.js

--- site/assets/js/countdown.js
+++ site/assets/js/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Dec 25, 2020").getTime();
+var countDownDate = new Date("Jan 1, 2021").getTime();
 
 // Update the count down every 1 second
 var countdownfunction = setInterval(function () {
