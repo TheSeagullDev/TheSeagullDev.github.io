af53b269b5b8ab758dfdcdbf88e63616afd5c7c6
Thingyman-DEV <67653706+Thingyman-DEV@users.noreply.github.com>
2021-11-04 16:16:34 -0400

    🎉 ✨ create new nuxt/tailwind site and landing page

---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="bg-blue-400 flex min-h-screen min-w-full">
+    <div
+      class="
+        bg-blue-500
+        text-center
+        w-1/3
+        text-2xl
+        m-auto
+        px-4
+        py-12
+        rounded-md
+      "
+    >
+      <p class="font-bold text-6xl py-2">Hello!</p>
+      <p class="text-xl inline">Welcome to my page! We're not ready for you</p>
+      <p class="text-xl italic inline">just</p>
+      <p class="text-xl inline">yet.</p>
+      <p class="text-xl">
+        Don't worry though! We're coming soon. Star this repostory on
+        <a
+          href="https://github.com/thingyman-dev/thingyman-dev.github.io"
+          class="text-blue-200"
+          >Github</a
+        >
+        to get updates.
+      </p>
+      <p class="text-xl">See you back here soon!</p>
+      <p class="text-xl">- Thingyman</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
